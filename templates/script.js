61aa9cb5caf6b1eef35364f9eecad815cd3d3856
Mehul Tandel <mehultandel.tech@gmail.com>
2022-01-22 15:12:01 +0530

    added bare minimum html pages

--- templates/script.js
+++
@@ -1,9 +0,0 @@
-const burgerMenu = document.getElementById('burger');
-const navLinks  = document.getElementsByClassName('nav-links')[0];
-burgerMenu.addEventListener("click",()=>{
-    if(navLinks.style.display === 'none'){
-        navLinks.style.display = 'block';
-    }else{
-        navLinks.style.display = 'none';
-    }
-})
